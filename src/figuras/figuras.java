ae574aaf7534d631146033685f0234ea6370ea48
Win10 <Win10@DESKTOP-24KUEVG>
2020-10-30 21:16:08 -0600

    commit para prueba

---
+++ src/figuras/figuras.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+/**
+ *
+ * @author Win10
+ */
+public class figuras {
+     
+    public static void main(String[] args){
+        int  c = 1;
+        int  f = 5;
+        
+        for (int i = 0; i < c; i++) {
+            for (int j = 0; j < f; j++) {
+                if (i == (c - 1)) {
+                    System.out.print("-");
+                } else {
+                    System.out.println(">");
+                }
+            }   
+}
+}
+}
