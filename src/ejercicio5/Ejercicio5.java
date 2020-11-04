65a19fd38909d004744d9a6d2aa8e80f4149fc55
Win10 <Win10@DESKTOP-24KUEVG>
2020-11-04 16:01:25 -0600

    commit 4, intento del Ejercicio 5

--- src/ejercicio5/Ejercicio5.java
+++ src/ejercicio5/Ejercicio5.java
@@ -11,14 +11,24 @@ package ejercicio5;
  */
 public class Ejercicio5 {
 
-  
-  
+    public static void main(String[] args) {
 
-    
-    public static void main (String[] args){
-    
-   
-    
+        int count = 50;
+        double pi = 0;
+        double denominator = 3;
+
+        for (int x = 0; x <= count; x++) {
+
+            if (x % 2 == 0) {
+                pi = pi + (4 / denominator);
+            } else {
+                pi = pi - (4 / denominator);
+            }
+            denominator = denominator + 2;
+        }
+        pi = pi * 4;
+        System.out.println(pi);
+
     }
-    
+
 }
