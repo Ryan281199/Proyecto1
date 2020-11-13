16874bedf984f26c70a142e2859ad8b991368bb2
Win10 <Win10@DESKTOP-24KUEVG>
2020-11-13 01:41:50 -0600

    commit 8, se añaden las figuras

--- src/figuras/figuras.java
+++ src/figuras/figuras.java
@@ -10,27 +10,74 @@ package figuras;
  * @author Win10
  */
 public class figuras {
-     
-    public static void main(String[] args){
-        int  c = 1;
-        int  f = 5;
-        
-        //figura1
-        for (int i = 0; i < c; i++) {
-            for (int j = 0; j < f; j++) {
-                if (i == (c - 1)) {
-                    System.out.print("-");
+
+    public static void main(String[] args) {
+
+        //figura 1
+        for (int i = 0; i < 1; i++) {
+            for (int n = 0; n < 5; n++) {
+                System.out.print("-");
+            }
+            System.out.println(">\n");
+        }
+
+        //figura 2   
+        for (int i = 0; i < 3; i++) {
+            for (int n = 0; n < 5; n++) {
+                System.out.print("-");
+            }
+            System.out.print(">\n");
+        }
+
+        //figura 3
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*");
                 } else {
-                    System.out.println(">");
+                    System.out.print(" ");
                 }
-            }   
-}
-        int co = 3;
-        int fi = 5;
-        for (int i=0; i<co; i++){
-        for(int j=0;j<fi; j++){
-        
+            }
+            System.out.println();
         }
+
+        //figura 4
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
-}
+
+        //figura 5
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5 || j == (6 - i)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        //figura 6
+        for (int i = 0; i < 5; i++) {
+
+            for (int n = 0; n < 5; n++) {
+                if ((i == 0) || (i == n) || (i == 5 - 1) || (n == 0) || (n == 5 - 1) || (n == 5 - i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
 }
